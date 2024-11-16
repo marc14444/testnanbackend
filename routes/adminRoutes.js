ff4d2c2e0f001409452f7cca3bab9c379119edad
marc14444 <amanijeanmarc41@gmail.com>
2024-11-16 22:26:11 +0000

    ordre des routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -23,21 +23,21 @@ router.post('/register', registerAdmin);
 router.post('/login', loginAdmin);
 router.put('/update/:id', authAdmin, updateAdmin);
 
+// Créer un nouvel employé
+router.post("/employes", authAdmin, upload.single('photoProfil'), creerEmploye);
+
 // Liste des employés
 router.get("/employes",authAdmin, listerEmployes);
 
+// Statistiques sur les employés (par exemple, nombre total d'employés)
+router.get("/employes/stats", authAdmin, getEmployeeStats);
+export default router;
+
 // Récupérer un employé par son id
 router.get("/employes/:id",authAdmin, getEmployeeById);
-
-// Créer un nouvel employé
-router.post("/employes", authAdmin, upload.single('photoProfil'), creerEmploye);
 
 // Modifier un employé
 router.put("/employes/:id",upload.single('photoProfil'),authAdmin, modifierEmploye);
 
 // Supprimer un employé
 router.delete("/employes/:id",authAdmin, supprimerEmploye);
-
-// Statistiques sur les employés (par exemple, nombre total d'employés)
-router.get("/employes/stats", authAdmin, getEmployeeStats);
-export default router;
